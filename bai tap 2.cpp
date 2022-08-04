37c8fe38cedda4cc3c3c2abe362a8cfbd6e62d4c
nguyvcuong <110459436+nguyvcuong@users.noreply.github.com>
2022-08-04 14:47:14 +0700

    C
    C

---
+++ bai tap 2.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+	
+	int a;
+	
+	printf("nhap so:");
+	scanf("%d",&a);
+	
+	if(a>0){
+		printf("la so nguyen duong");
+		
+	}else{
+	
+		
+		printf("la so am");
+	
+	}if(a=0){
+		printf("so 0");
+	}
+}
