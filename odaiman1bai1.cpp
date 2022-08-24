3694a3601f72582fcb39014d53bbada5579465ec
nguyvcuong <110459436+nguyvcuong@users.noreply.github.com>
2022-08-24 08:58:54 +0700

    odaiman1

---
+++ odaiman1bai1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+int a, b;
+printf("Nhap so a: ");
+scanf("%d", &a);
+printf("Nhap so b: ");
+scanf("%d", &b);
+printf("So lon hon  a la %d\n", a);
+printf("So nho hon b la %d\n", b);
+}
